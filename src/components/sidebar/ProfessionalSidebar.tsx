c6be624a1578691a275c8b9e4c97abb82145966d
Anjali-Prajapati22 <anjali.prajapati@orionik.com>
2026-05-13 17:21:57 +0530

    Designed professional side bar

--- src/components/sidebar/ProfessionalSidebar.tsx
+++ src/components/sidebar/ProfessionalSidebar.tsx
@@ -1,13 +1,12 @@
-import { MoveRight, X } from "lucide-react";
+import { ChevronRight, Clock3, Package2 } from "lucide-react";
+
+import { useSelector } from "react-redux";
 
 import { getUserName, CurrencyIcon } from "@/utils";
 
-import {
-  Staff,
-  Step,
-  Service,
-} from "@/types";
-import { JSX } from "react";
+import { Staff, Step, Service, OutletRootState } from "@/types";
+
+import { RootState } from "@/store";
 
 interface SelectedStaffService extends Service {
   qty: number;
@@ -29,93 +28,146 @@ const ProfessionalSidebar = ({
   totalPrice,
   totalDuration,
   goToStep,
-}: ProfessionalSidebarProps): JSX.Element => {
+}: ProfessionalSidebarProps) => {
+  const { outletName } = useSelector(
+    (state: OutletRootState) => state.outletDetails,
+  );
+
+  const { isService } = useSelector((state: RootState) => state.outletDetails);
+
   return (
     <>
-      <p className="font-bebas text-xl mb-3">Your Order</p>
+      {/* Header */}
+      <div className="p-3 border-b border-gray-300">
+        <p className="font-black uppercase tracking-tight text-2xl mb-0">
+          Your Order
+        </p>
 
-      {pro?.id && (
-        <div className="flex items-center gap-3 mb-3 p-3 border border-border rounded bg-white">
-          {pro.imageUrl ? (
-            <img
-              src={pro.imageUrl}
-              alt={pro.name}
-              className="w-10 h-10 rounded-full object-cover"
-            />
-          ) : (
-            <div
-              className="w-10 h-10 rounded-full bg-black text-white flex items-center justify-center"
-              style={{ background: pro.color || "#111" }}
-            >
-              {getUserName(pro.name)}
-            </div>
+        <p className="text-xs font-semibold text-black/40 uppercase line-clamp-1">
+          {outletName || "-"} Outlet
+        </p>
+      </div>
+
+      <div className="flex flex-col justify-between h-full">
+        {/* Content */}
+        <div className="p-3">
+          {/* Professional */}
+          {pro?.id && (
+            <>
+              <p className="text-sm font-semibold my-1.5 text-black/40 uppercase">
+                Selected Professional
+              </p>
+
+              <div className="flex items-center gap-3 mb-4 py-2 px-3 border border-black/20 rounded">
+                {pro.imageUrl ? (
+                  <img
+                    src={pro.imageUrl}
+                    alt={pro.name}
+                    className="w-10 h-10 rounded-md object-cover"
+                  />
+                ) : (
+                  <div
+                    className="w-10 h-10 rounded-md bg-btn-bg/50 text-white flex items-center justify-center"
+                    style={{
+                      background: pro.color,
+                    }}
+                  >
+                    {getUserName(pro.name)}
+                  </div>
+                )}
+
+                <div>
+                  <p className="text-sm font-bold uppercase line-clamp-1">
+                    {pro.name}
+                  </p>
+
+                  <p className="text-xs text-gray-500 uppercase line-clamp-1">
+                    {pro.staff_type}
+                  </p>
+                </div>
+              </div>
+            </>
           )}
 
-          <div>
-            <p className="text-sm font-semibold">{pro.name}</p>
+          {/* Services */}
+          <p className="text-sm font-semibold my-1.5 text-black/40 uppercase">
+            Selected Services
+          </p>
 
-            <p className="text-xs text-gray-500">
-              {pro.staff_type}
-            </p>
-          </div>
-        </div>
-      )}
-
-      {selectedStaffServices?.length > 0 && (
-        <ul className="md:h-[calc(100dvh-70px)] h-[calc(100dvh-230px)] overflow-y-scroll mb-1 scrollbar-none">
-          {selectedStaffServices?.map(
-            (svc: SelectedStaffService, index: number) => (
-              <li
-                key={svc.id}
-                className={`flex justify-between text-sm py-2 ${index !== selectedStaffServices.length - 1
-                  ? "border-b border-dotted border-gray-400"
-                  : ""
+          {selectedStaffServices?.length > 0 && (
+            <ul className="md:h-[calc(100dvh-410px)] h-[calc(100dvh-260px)] overflow-y-scroll scrollbar-none">
+              {selectedStaffServices.map((svc, index) => (
+                <li
+                  key={svc.id}
+                  className={`flex justify-start flex-col items-start text-sm py-2 ${
+                    index !== selectedStaffServices.length - 1
+                      ? "border-b border-dotted border-gray-400"
+                      : ""
                   }`}
-              >
-                <span className="flex justify-between flex-row items-center gap-1">
-                  {svc.name} <X size={12} /> {svc.qty} (
-                  {svc.min_time || svc.estimated_time} min)
-                </span>
+                >
+                  <p className="flex justify-between flex-row items-center gap-1 line-clamp-1 text-sm font-medium uppercase">
+                    {svc.name}
+                  </p>
 
-                <span className="flex flex-row items-center">
-                  <CurrencyIcon size={12} />
-                  {svc.price || svc.min_price}
-                </span>
-              </li>
-            ),
+                  <div className="grid grid-cols-3 items-center gap-2 w-full">
+                    {/* Qty */}
+                    <div className="flex items-center justify-start gap-1 text-xs text-black/60 font-medium">
+                      <Package2 size={13} />
+                      <span>{svc.qty}</span>
+                    </div>
+
+                    {/* Duration */}
+                    <div className="flex items-center justify-center gap-1 text-xs text-black/60 font-medium">
+                      <Clock3 size={13} />
+                      <span>{svc.min_time || svc.estimated_time} min</span>
+                    </div>
+
+                    {/* Price */}
+                    <p className="flex items-center justify-end gap-1 text-xs text-black/60 font-medium">
+                      <CurrencyIcon size={12} />
+                      {svc.price || svc.min_price}
+                    </p>
+                  </div>
+                </li>
+              ))}
+            </ul>
           )}
-        </ul>
-      )}
+        </div>
 
-      <div className="h-px bg-border mt-auto my-2.5" />
+        {/* Footer */}
+        <div className="p-3 border-t border-gray-300">
+          <div className="flex justify-between items-end mb-1">
+            <span className="font-medium text-sm text-black/60 uppercase">
+              Subtotal
+            </span>
 
-      <div className="flex justify-between text-lg mb-0.5">
-        <span className="font-semibold">Subtotal</span>
+            <span className="font-mono font-semibold flex flex-row items-center">
+              <CurrencyIcon size={18} />
+              {totalPrice}
+            </span>
+          </div>
 
-        <span className="font-mono font-semibold flex flex-row items-center">
-          <CurrencyIcon size={18} />
-          {totalPrice}
-        </span>
-      </div>
+          <div className="flex justify-between items-end mb-1">
+            <span className="font-medium text-sm text-black/60 uppercase">
+              Duration
+            </span>
 
-      <div className="flex justify-between text-base">
-        <span className="font-semibold">Total Duration</span>
+            <span className="font-mono font-semibold">
+              {totalDuration} mins
+            </span>
+          </div>
 
-        <span className="font-mono font-semibold">
-          {totalDuration} mins
-        </span>
-      </div>
-
-      <div>
-        <button
-          disabled={!selectedStaffServices?.length}
-          onClick={() => goToStep("time")}
-          className="bg-btn-bg text-btn-text border-btn-bg px-2 w-full relative py-3 font-dm text-sm font-bold tracking-[1.5px] uppercase cursor-pointer mt-3.5 rounded-sm transition-all duration-200 disabled:bg-[#ccc] disabled:cursor-not-allowed max-md:py-3.5 max-md:text-xs"
-        >
-          <span className="flex flex-row justify-center items-center gap-2">
-            Choose Time <MoveRight />
-          </span>
-        </button>
+          <button
+            disabled={!selectedStaffServices.length}
+            onClick={() => goToStep("time")}
+            className="bg-btn-bg text-btn-text border-btn-bg hover:text-btn-text-hover hover:border-btn-bg-hover hover:bg-btn-bg-hover px-2 w-full relative py-3 font-dm text-xs font-bold tracking-[1.5px] uppercase cursor-pointer mt-3.5 transition-all duration-200 disabled:bg-btn-bg-hover/70 disabled:cursor-not-allowed max-md:py-3.5 max-md:text-xs"
+          >
+            <span className="flex flex-row justify-center items-center gap-2">
+              {isService ? "Choose Time" : "Continue"}
+              <ChevronRight size={16} />
+            </span>
+          </button>
+        </div>
       </div>
     </>
   );
