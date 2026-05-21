68a81f91759706f25a81946cfb3fedcfd06ab514
Hardik Dongre <hardik.dongare@orionik.com>
2026-05-21 14:38:53 +0530

    update sidebar UI and all

--- src/components/common/MainLayout.tsx
+++ src/components/common/MainLayout.tsx
@@ -6,40 +6,25 @@ export default function MainLayout({
     sidebar,
     renderButton,
     isConfirm = false,
-    isSidebarOpen = false
+    isSidebarOpen = false,
+    handleSidebarOpen,
 }: MainLayoutProps) {
     const { selectedServices, selectedProfessional } = useSelector(
         (state: OutletRootState) => state.booking.service,
     );
-
-    const isOpen = selectedServices?.length > 0 || !!selectedProfessional?.id;
-
+    const isDesktopSidebarOpen = selectedServices?.length > 0 || !!selectedProfessional?.id;
     return (
         <div className="aaravpos-main-layout">
-            <div className="aaravpos-layout-wrapper ">
+            <div className={isDesktopSidebarOpen ? "aaravpos-layout-wrapper" : ""}>
                 <main className="aaravpos-main-content">
                     {children}
-
-                    {isConfirm ? (
-                        <button className="aaravpos-btn">
-                            <span>View Order</span>
-                        </button>
-                    ) : (
-                        renderButton
-                    )}
+                    {isConfirm ? <button className="aaravpos-btn" onClick={handleSidebarOpen}><span>View Order</span></button> : renderButton}
                 </main>
-
-                <aside
-                    className={`aaravpos-barber-sidebar ${isOpen ? "open" : ""}`}
-                >
-                    {sidebar}
-                </aside>
+                <aside className={`aaravpos-sidebar ${isDesktopSidebarOpen ? "open" : ""}`}>{sidebar}</aside>
             </div>
             {isSidebarOpen && (
-                <div className="aaravpos-barber-sidebar-mobile">
-                    <div className="aaravpos-barber-sidebar-mobile-content">
-                        {sidebar}
-                    </div>
+                <div className="aaravpos-sidebar-mobile">
+                    <div className="aaravpos-sidebar-mobile-content">{sidebar}</div>
                 </div>
             )}
         </div>
