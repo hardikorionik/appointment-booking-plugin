b5d6497e3c84444f88554a9db1d648ca2a77c39e
Hardik Dongre <hardik.dongare@orionik.com>
2026-05-25 16:19:38 +0530

    update pakage json for remove tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 import dts from "vite-plugin-dts";
 import path, { resolve, dirname } from "path";
 import { fileURLToPath } from "url";
@@ -10,7 +9,6 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
     dts({
       include: ["src/components", "src/index.ts"],
       exclude: ["src/main.tsx", "src/App.tsx"],
@@ -42,8 +40,6 @@ export default defineConfig({
         "redux",
         "react-redux",
         "redux-persist",
-        'tailwindcss',
-
         // add these
         "lodash",
         "luxon",
